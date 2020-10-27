d71f48dd7f1bcf0a1892fd000915c09c69bae8f1
harrytaorui <rui.tao@thoughtworks.com>
2020-10-27 22:10:49 +0800

    [taorui]:添加成员功能实现

--- src/App/Component/Group.js
+++ src/App/Component/Group.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
 
 export default class Group extends Component {
   // eslint-disable-next-line react/state-in-constructor
   state = {
     members: {},
     isLoading: true,
+    name: '',
+    isOpen: false,
   };
 
   async componentDidMount() {
@@ -13,21 +16,77 @@ export default class Group extends Component {
     this.setState({ members, isLoading: false });
   }
 
+  handleAddMember = async () => {
+    const member = {
+      name: this.state.name,
+    };
+    await fetch('http://localhost:8080/members', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(member),
+    });
+    this.setState({ isOpen: false });
+  };
+
+  handleFieldChange = (field, event) => {
+    const { value } = event.target;
+    this.setState({
+      [field]: value,
+    });
+  };
+
+  toggleModal = () => {
+    // eslint-disable-next-line react/no-access-state-in-setstate
+    const isOpen = !this.state.isOpen;
+    this.setState({
+      isOpen,
+    });
+  };
+
   render() {
     if (this.state.isLoading) {
       return <p>加载中</p>;
     }
-    const { members } = this.state;
+    const { members, isOpen } = this.state;
     return (
-      <div className="member-list">
-        <h1>学员列表</h1>
-        {members.map((member, index) => {
-          return (
-            <div key={index}>
-              {member.id}.{member.name}
-            </div>
-          );
-        })}
+      <div className="member-page">
+        <div className="member-list">
+          <h1>学员列表</h1>
+          {members.map((member, index) => {
+            return (
+              <div key={index}>
+                {member.id}.{member.name}
+              </div>
+            );
+          })}
+        </div>
+        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+        <div className="add-member" onClick={this.toggleModal}>
+          + 添加学员
+        </div>
+        <Modal isOpen={isOpen} className="add-modal">
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            value={this.state.name}
+            placeholder="请输入姓名"
+            onChange={(event) => this.handleFieldChange('name', event)}
+          />
+          <button
+            type="submit"
+            className="btn btn-primary"
+            disabled={this.state.name === ''}
+            onClick={() => {
+              this.handleAddMember();
+            }}
+          >
+            Enter
+          </button>
+        </Modal>
       </div>
     );
   }
